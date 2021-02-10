7a670870719f4044ed6267fcf8bb4574d16ef023
PhilvisThirteen <53509443+PhilvisThirteen@users.noreply.github.com>
2021-02-10 13:27:49 +0800

    update

--- webpack.config.ts
+++ webpack.config.ts
@@ -53,7 +53,7 @@ const webpackConfig = (env): Configuration => ({
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: "./public/index.html"
+			template: "./public/index.html",
 		}),
 		new webpack.DefinePlugin({
 			"process.env.PRODUCTION": env.production || !env.development,
